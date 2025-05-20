0575f0d1d95906bbf11c4e15f4867b3c84674311
Neeraj Suyal <suyalneeraj09@gmail.com>
2025-05-20 08:12:59 +0530

    changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4db64edba5cbd43127a50f4b29667267
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "57f1e9d396a1fa33fa3cb8857f058ff5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2cf3c7a471d18d8496a44b93a069109a",
+"flutter_bootstrap.js": "0a592336bbfebe99b3dd8331b44c85b8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
